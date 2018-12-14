8427b96c3437e5859a36b67a400b6c2efac00f7a
Adi <adrian169919@gmail.com>
2018-12-14 20:12:02 +0100

    Update login_p.php

--- AnimeMoon_live/login_p.php
+++ AnimeMoon_live/login_p.php
@@ -8,7 +8,7 @@ all rights reserved @2017
 ||||||||||||||||||||||||||||||||||||||
 -->
 
-<?php 
+<?php
 //db connection
     require 'db.php';
     session_start();
@@ -24,19 +24,19 @@ all rights reserved @2017
   <script src="js/jquery-3.1.1.min.js"></script>
 </head>
 
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if (isset($_POST['login'])) { //user logging in
 
         require 'login.php';
-        
+
     }
 }
 ?>
 
 <body class="dlx_ov_hidden">
-  
+
         <!-- dodaje menu główne -->
     <?php require  ("menu.php"); ?>
 
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
           <div class="form-group">
             <label for="inputEmail" class="col-sm-2 control-label">Email</label>
             <div class="col-sm-10">
-              <input type="email" class="form-control" id="inputEmail" required autocomplete="off" name="email" placeholder="Haku_@animemoon.com">
+              <input type="email" class="form-control" id="inputEmail" required autocomplete="off" name="email" placeholder="Example_@animemoon.com">
             </div>
           </div>
           <div class="form-group">
@@ -60,14 +60,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
               <input type="password" class="form-control" id="inputPassword" required autocomplete="off" name="password" placeholder="Hasło">
             </div>
           </div>
-           
+
             <div class="col-sm-offset-7 col-sm-10">
               <p><a href="forgot.php">Nie pamiętam hasła</a></p>
             </div>
 
           <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
-              <button type="submit" class="btn btn-success" name="login">Zaloguj mnię</button>
+              <button type="submit" class="btn btn-success" name="login">Zaloguj mnie</button>
             </div>
           </div>
         </form>
@@ -77,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <img src="img/baner1.png" alt="anime_footer" class="baner img-responsive">
 
 
-  <script src="js/index.js"></script>  
+  <script src="js/index.js"></script>
   <script src="js/bootstrap.min.js"></script>
 
 

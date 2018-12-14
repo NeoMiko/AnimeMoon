362cf11c8b1e18a9c277e0a4f76a03610c4da092
NeoMiko <kamil_tchoryk@hotmail.com>
2018-12-14 21:53:38 +0100

    fix 0.0.2

--- AnimeMoon_live/register.php
+++ AnimeMoon_live/register.php
@@ -1,5 +1,5 @@
 <?php
-/* Registration process, inserts user info into the database 
+/* Registration process, inserts user info into the database
    and sends account confirmation email message
  */
 
@@ -14,21 +14,21 @@ $last_name = $mysqli->escape_string($_POST['lastname']);
 $email = $mysqli->escape_string($_POST['email']);
 $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
-      
+
 // Check if user with that email already exists
 $result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
 
 // We know user email exists if the rows returned are more than 0
 if ( $result->num_rows > 0 ) {
-    
+
     $_SESSION['message'] = 'Użytkownik z tym e-mailem już istnieje!';
     header("location: error.php");
-    
+
 }
 else { // Email doesn't already exist in a database, proceed...
 
     // active is 0 by DEFAULT (no need to include it here)
-    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) " 
+    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) "
             . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
 
     // Add user to the database
@@ -37,13 +37,13 @@ else { // Email doesn't already exist in a database, proceed...
         $_SESSION['active'] = 0; //0 until user activates their account with verify.php
         $_SESSION['logged_in'] = true; // So we know the user has logged in
         $_SESSION['message'] =
-                
+
                  "Wysłano link potwierdzenia $email, proszę zweryfikuj
                   swoje konto, klikając link w wiadomości!";
 
         // Send registration confirmation link (verify.php)
         $to      = $email;
-        $subject = 'Weryfikacja konta ( clevertechie.com )';
+        $subject = 'Weryfikacja konta ( animemoon.com )';
         $message_body = '
         Witam '.$first_name.',
 
@@ -51,11 +51,11 @@ else { // Email doesn't already exist in a database, proceed...
 
         Kliknij ten link, aby aktywować konto:
 
-        http://localhost/login-system/verify.php?email='.$email.'&hash='.$hash;  
+        http://localhost/login-system/verify.php?email='.$email.'&hash='.$hash;
 
         mail( $to, $subject, $message_body );
 
-        header("location: profile.php"); 
+        header("location: profile.php");
 
     }
 
